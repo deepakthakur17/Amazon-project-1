1368a68c127eab20e77bece0d7cdaf014fc6a6c8
Deepak <deepakthakur1661999@gmail.com>
2024-06-24 14:57:38 +0530

    commit

---
+++ mobile.js
@@ -0,0 +1,59 @@
+const sidebarNavigationEl = document.getElementById("sidebar-container-navigation")
+const sidebarOpenNavigationEl = document.getElementById("nav-link")
+const sidebarCloseNavigationEl = document.getElementById("sidebar-navigation-close")
+
+sidebarOpenNavigationEl.addEventListener("click", () => {
+    sidebarNavigationEl.classList.toggle("sliderbar-show")
+
+})
+sidebarCloseNavigationEl.addEventListener("click", () => {
+    sidebarNavigationEl.classList.toggle("sliderbar-show")
+
+})
+
+
+var index = 1
+slideFun(index)
+
+function addSlide(num) {
+    index = index + num
+    slideFun(index)
+}
+
+function slideFun(idx) {
+    var allDivs = document.querySelectorAll(".slides")
+
+
+
+    if (idx <= 0) {
+        index = allDivs.length
+    }
+
+    if (idx > allDivs.length) {
+        index = 1
+    }
+
+    for (var i = 0; i < allDivs.length; i++) {
+        allDivs[i].style.display = "none"
+    }
+
+    allDivs[index - 1].style.display = "block"
+}
+
+// SIGN BUTTON 
+
+function SignPage() {
+    window.location.href = "index-2.html"
+}
+
+
+
+//how to automate above slider
+
+function automateSlide() {
+    addSlide(1)
+
+    //call above function after each 4 seconds
+    setTimeout(automateSlide, 4000)
+}
+automateSlide()
